b43fd1b66696e62b434c09f46f60ff7f42cc6cad
berk-karaal <karaalberk7@gmail.com>
2024-08-17 11:50:53 +0300

    feat(restapi): use LogHandler middleware

--- backend/cmd/restapi/main.go
+++ backend/cmd/restapi/main.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"github.com/berk-karaal/letuspass/backend/internal/logging"
+	"github.com/berk-karaal/letuspass/backend/internal/middlewares"
 	"github.com/berk-karaal/letuspass/backend/internal/routes"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	engine := gin.Default()
+	logger := logging.NewLogger()
+
+	engine := gin.New()
+	engine.Use(gin.Recovery())
+	engine.Use(middlewares.LogHandler(logger))
 	routes.SetupRoutes(engine)
+
 	engine.Run()
 }
