24dc6345556d756ee60ed708e1b4d357c1227048
berk-karaal <karaalberk7@gmail.com>
2024-08-19 12:01:24 +0300

    feat(middleware): create CurrentUserHandler middleware and ExtractUserFromGinContext helper function

---
+++ backend/internal/middlewares/current_user.go
@@ -0,0 +1,74 @@
+package middlewares
+
+import (
+	"errors"
+	"net/http"
+	"time"
+
+	"github.com/berk-karaal/letuspass/backend/internal/common/logging"
+	"github.com/berk-karaal/letuspass/backend/internal/models"
+	"github.com/gin-gonic/gin"
+	"github.com/rs/zerolog"
+	"gorm.io/gorm"
+)
+
+const UserContextKey = "user"
+
+// CurrentUserHandler middleware checks request for the authenticated user and puts user data to Gin context.
+// If request sent without authentication, this middleware aborts with HTTP 401 Unauthorized with no response
+// body. This middleware should only be used on routes that required authentication.
+func CurrentUserHandler(logger *logging.Logger, db *gorm.DB) func(c *gin.Context) {
+	return func(c *gin.Context) {
+		sessionToken, err := c.Cookie("session_token") // TODO: get from config
+		if err != nil {
+			if errors.Is(err, http.ErrNoCookie) {
+				c.AbortWithStatus(http.StatusUnauthorized)
+				return
+			}
+			logger.RequestEvent(zerolog.ErrorLevel, c).Err(err).Msg("Checking session cookie failed.")
+			c.AbortWithStatus(http.StatusInternalServerError)
+			return
+		}
+
+		var userSession models.UserSession
+		err = db.First(&userSession, "token = ?", sessionToken).Error
+		if err != nil {
+			if errors.Is(err, gorm.ErrRecordNotFound) {
+				c.AbortWithStatus(http.StatusUnauthorized)
+				return
+			}
+			logger.RequestEvent(zerolog.ErrorLevel, c).Err(err).Msg("Querying session token failed.")
+			c.AbortWithStatus(http.StatusInternalServerError)
+			return
+		}
+
+		err = db.Model(&userSession).Update("expires_at", time.Now().Add(time.Minute*60*24)).Error
+		if err != nil {
+			logger.RequestEvent(zerolog.ErrorLevel, c).Err(err).Msg("Extending user session expire time failed.")
+		}
+
+		var user models.User
+		err = db.First(&user, "id = ?", userSession.UserID).Error
+		if err != nil {
+			logger.RequestEvent(zerolog.ErrorLevel, c).Err(err).Msg("Querying user failed.")
+			c.AbortWithStatus(http.StatusInternalServerError)
+			return
+		}
+
+		c.Set(UserContextKey, user)
+	}
+}
+
+// ExtractUserFromGinContext returns User model inserted to Gin context by CurrentUserHandler middleware.
+// This helper function is used to reduce boilerplate code.
+func ExtractUserFromGinContext(c *gin.Context) (models.User, bool) {
+	val, ok := c.Get(UserContextKey)
+	if !ok {
+		return models.User{}, false
+	}
+	user, ok := val.(models.User)
+	if !ok {
+		return models.User{}, false
+	}
+	return user, true
+}
