8cf315b2f6081cc9ccfecba446ba5e13fbe7bbdc
berk-karaal <karaalberk7@gmail.com>
2024-08-21 13:40:51 +0300

    feat: create retrieve vault endpoint (api/v1/vaults/:id)

--- backend/internal/controllers/vault.go
+++ backend/internal/controllers/vault.go
@@ -2,11 +2,14 @@ package controllers
 
 import (
 	"net/http"
+	"strconv"
+	"time"
 
 	"github.com/berk-karaal/letuspass/backend/internal/common/bodybinder"
 	"github.com/berk-karaal/letuspass/backend/internal/common/logging"
 	"github.com/berk-karaal/letuspass/backend/internal/middlewares"
 	"github.com/berk-karaal/letuspass/backend/internal/models"
+	"github.com/berk-karaal/letuspass/backend/internal/schemas"
 	"github.com/gin-gonic/gin"
 	"github.com/rs/zerolog"
 	"gorm.io/gorm"
@@ -68,5 +71,71 @@ func HandleVaultsCreate(logger *logging.Logger, db *gorm.DB) func(c *gin.Context
 		// TODO: audit log
 
 		c.JSON(http.StatusCreated, VaultCreateResponse{Id: vault.ID, Name: vault.Name})
+	}
+}
+
+// HandleVaultsRetrieve
+//
+//	@Summary	Retrieve vault by id
+//	@Tags		vaults
+//	@Produce	json
+//	@Success	200	{object}	controllers.HandleVaultsCreate.VaultCreateResponse
+//	@Failure	400	{object}	schemas.BadRequestResponse
+//	@Failure	401
+//	@Forbidden	403
+//	@Failure	500
+//	@Router		/vaults/{id} [get]
+//	@Param		id	path	int	true	"Vault id"
+func HandleVaultsRetrieve(logger *logging.Logger, db *gorm.DB) func(c *gin.Context) {
+
+	type VaultRetrieveResponse struct {
+		Id        uint      `json:"id"`
+		Name      string    `json:"name"`
+		CreatedAt time.Time `json:"created_at"`
+	}
+
+	return func(c *gin.Context) {
+		vaultId, err := strconv.Atoi(c.Param("id"))
+		if err != nil {
+			c.JSON(http.StatusBadRequest, schemas.BadRequestResponse{Error: "Id must be an integer."})
+			return
+		}
+
+		user, ok := middlewares.ExtractUserFromGinContext(c)
+		if !ok {
+			logger.RequestEvent(zerolog.ErrorLevel, c).Msg("Extracting user from Gin context failed.")
+			c.Status(http.StatusInternalServerError)
+			return
+		}
+
+		var hasReadPerm bool
+		err = db.Model(&models.VaultPermission{}).Select("count(*) > 0").
+			Where("vault_id = ? AND user_id = ? AND permission = ?", vaultId, user.ID, models.VaultPermissionRead).
+			Scan(&hasReadPerm).Error
+		if err != nil {
+			logger.RequestEvent(zerolog.ErrorLevel, c).Err(err).Msg("Checking vault permissions of user failed.")
+			c.Status(http.StatusInternalServerError)
+			return
+		}
+		if !hasReadPerm {
+			c.Status(http.StatusForbidden)
+			return
+		}
+
+		var vault models.Vault
+		err = db.First(&vault, vaultId).Error
+		if err != nil {
+			// We don't check if err is gorm.ErrRecordNotFound because we already checked it while checking
+			// user's vault permissions.
+			logger.RequestEvent(zerolog.ErrorLevel, c).Err(err).Msg("Retrieving vault from db failed.")
+			c.Status(http.StatusInternalServerError)
+			return
+		}
+
+		c.JSON(http.StatusOK, VaultRetrieveResponse{
+			Id:        vault.ID,
+			Name:      vault.Name,
+			CreatedAt: vault.CreatedAt,
+		})
 	}
 }
