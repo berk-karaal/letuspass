d179e8ae817a475803e4d0a4f4f675bf8142eb34
berk-karaal <karaalberk7@gmail.com>
2024-08-21 22:20:34 +0300

    feat: create pagination common package

---
+++ backend/internal/common/pagination/pagination.go
@@ -0,0 +1,35 @@
+package pagination
+
+import (
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
+
+const maxPageSize = 100
+
+func Paginate(c *gin.Context) func(db *gorm.DB) *gorm.DB {
+	return func(db *gorm.DB) *gorm.DB {
+		page, _ := strconv.Atoi(c.Query("page"))
+		if page <= 0 {
+			page = 1
+		}
+
+		pageSize, _ := strconv.Atoi(c.Query("page_size"))
+		switch {
+		case pageSize > maxPageSize:
+			pageSize = maxPageSize
+		case pageSize <= 0:
+			pageSize = 10
+		}
+
+		offset := (page - 1) * pageSize
+		return db.Offset(offset).Limit(pageSize)
+	}
+}
+
+type StandardPaginationResponse[T any] struct {
+	Results []T `json:"results"`
+	Count   int `json:"count"`
+}
