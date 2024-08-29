4d3f8f3ba2aa4a14f1a0cbb1ebce7276f52a4cae
berk-karaal <karaalberk7@gmail.com>
2024-08-29 23:56:28 +0300

    feat: add react-query to project

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,16 +1,22 @@
+import App from "@/App.tsx";
 import { createTheme, MantineProvider } from "@mantine/core";
 import "@mantine/core/styles.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import App from "@/App.tsx";
-
 const theme = createTheme({});
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <MantineProvider theme={theme}>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </MantineProvider>
   </StrictMode>
 );
