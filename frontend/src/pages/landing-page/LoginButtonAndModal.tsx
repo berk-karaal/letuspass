eb2aac90c770b670b9c08a6c71b08a035cebb1e2
berk-karaal <karaalberk7@gmail.com>
2024-08-30 00:32:58 +0300

    feat(landing-page): implement login api request flow

--- frontend/src/pages/landing-page/LoginButtonAndModal.tsx
+++ frontend/src/pages/landing-page/LoginButtonAndModal.tsx
@@ -1,8 +1,21 @@
-import { Button, Group, Modal, PasswordInput, TextInput } from "@mantine/core";
+import { authLogin } from "@/api/letuspass";
+import { SchemasBadRequestResponse } from "@/api/letuspass.schemas";
+import {
+  Button,
+  Group,
+  Modal,
+  PasswordInput,
+  Text,
+  TextInput,
+} from "@mantine/core";
 import { useForm } from "@mantine/form";
 import { useDisclosure } from "@mantine/hooks";
+import { useMutation } from "@tanstack/react-query";
+import { isAxiosError } from "axios";
+import { useState } from "react";
 
 export default function LoginButtonAndModal() {
+  const [errorText, setErrorText] = useState<string | null>(null);
   const [opened, { open, close }] = useDisclosure(false);
 
   const form = useForm({
@@ -19,8 +32,30 @@ export default function LoginButtonAndModal() {
   });
 
   const handleSubmit = (values: typeof form.values) => {
-    console.log(values);
+    setErrorText(null);
+    mutation.mutate({ email: values.email, password: values.password });
   };
+
+  const mutation = useMutation({
+    mutationFn: authLogin,
+    onSuccess: (data) => {
+      console.log("Login success", data);
+    },
+    onError: (error) => {
+      if (isAxiosError(error)) {
+        switch (error.response?.status) {
+          case 400:
+            let resp = error.response.data as SchemasBadRequestResponse;
+            setErrorText(resp.error);
+            break;
+          default:
+            setErrorText("An error occurred. Please try again later.");
+        }
+      } else {
+        setErrorText("An error occurred. Please try again later.");
+      }
+    },
+  });
 
   return (
     <>
@@ -32,17 +67,25 @@ export default function LoginButtonAndModal() {
             placeholder="your@email.com"
             key={form.key("email")}
             {...form.getInputProps("email")}
+            disabled={mutation.isPending}
           />
           <PasswordInput
             withAsterisk
             label="Password"
             key={form.key("password")}
             {...form.getInputProps("password")}
+            disabled={mutation.isPending}
             mt={"xs"}
           />
 
+          <Text c={"red"} mt={"xs"} display={errorText ? "block" : "none"}>
+            {errorText}
+          </Text>
+
           <Group justify="flex-end" mt="md">
-            <Button type="submit">Login</Button>
+            <Button type="submit" loading={mutation.isPending}>
+              Login
+            </Button>
           </Group>
         </form>
       </Modal>
