1663a450da2d724ff704ab00dfb3beda63de94d9
berk-karaal <karaalberk7@gmail.com>
2024-08-30 01:29:08 +0300

    feat: implement ProtectedRoute logic

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,59 @@
+import { getCurrentUser } from "@/api/letuspass";
+import router from "@/routes";
+import { useAppDispatch } from "@/store/hooks";
+import { startupComplete, userLoggedIn } from "@/store/slices/user";
+import { notifications } from "@mantine/notifications";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { useEffect } from "react";
 import { RouterProvider } from "react-router-dom";
 
-import router from "@/routes";
-
 function App() {
+  const dispatch = useAppDispatch();
+
+  const currentUserQuery = useQuery({
+    queryKey: ["current-user"],
+    queryFn: getCurrentUser,
+    refetchOnWindowFocus: false,
+    staleTime: Infinity,
+    gcTime: Infinity,
+    retry: (failureCount: number, error: Error) => {
+      if (failureCount > 2) {
+        return false;
+      }
+      if (axios.isAxiosError(error)) {
+        if (error.response?.status == 401) {
+          return false;
+        }
+      }
+      return true;
+    },
+  });
+
+  useEffect(() => {
+    if (currentUserQuery.isError) {
+      dispatch(startupComplete());
+      if (
+        axios.isAxiosError(currentUserQuery.error) &&
+        currentUserQuery.error.response?.status !== 401
+      ) {
+        notifications.show({
+          title: "Failed to fetch current user",
+          message: "Failed to fetch current user.",
+          color: "red",
+        });
+      }
+    } else if (currentUserQuery.data) {
+      dispatch(startupComplete());
+      dispatch(
+        userLoggedIn({
+          email: currentUserQuery.data.email,
+          name: currentUserQuery.data.name,
+        })
+      );
+    }
+  }, [currentUserQuery, dispatch]);
+
   return (
     <>
       <RouterProvider router={router} />
--- frontend/src/pages/ProtectedRoute/index.tsx
+++ frontend/src/pages/ProtectedRoute/index.tsx
@@ -1,10 +1,15 @@
+import { useAppSelector } from "@/store/hooks";
 import { Navigate, Outlet } from "react-router-dom";
 
 const ProtectedRoute = () => {
-  const user = { isAuthenticated: true };
+  const user = useAppSelector((state) => state.user);
+
+  if (!user.startupComplete) {
+    return <div>Loading...</div>;
+  }
 
   if (!user.isAuthenticated) {
-    return <Navigate to="/login" replace />;
+    return <Navigate to="/" replace />;
   }
 
   return <Outlet />;
