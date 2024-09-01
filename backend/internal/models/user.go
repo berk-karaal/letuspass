c9a26784e0fa05cf50d02d7661eefd674066980e
berk-karaal <karaalberk7@gmail.com>
2024-09-01 19:03:17 +0300

    feat(models): add KeyDerivationSalt and PublicKey fields to User model

--- backend/internal/models/user.go
+++ backend/internal/models/user.go
@@ -4,10 +4,12 @@ import "gorm.io/gorm"
 
 type User struct {
 	gorm.Model
-	Email    string `gorm:"unique;index"`
-	Password string
-	Name     string
-	IsActive bool `gorm:"default:true"`
+	Email             string `gorm:"unique;index"`
+	Password          string
+	Name              string
+	KeyDerivationSalt string
+	PublicKey         string
+	IsActive          bool `gorm:"default:true"`
 
 	UserSessions     []UserSession
 	VaultPermissions []VaultPermission
