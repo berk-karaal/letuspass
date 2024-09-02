351159e03956f80d501439300a193b40555d995b
berk-karaal <karaalberk7@gmail.com>
2024-09-02 20:36:41 +0300

    feat(vault-users-page): implement base user listing with no functionality

---
+++ frontend/src/pages/vault-users-page/VaultUsersList/index.tsx
@@ -0,0 +1,94 @@
+import { listVaultUsers } from "@/api/letuspass";
+import { useAppSelector } from "@/store/hooks";
+import {
+  ActionIcon,
+  Box,
+  Group,
+  Loader,
+  Text,
+  useMantineColorScheme,
+  useMantineTheme,
+} from "@mantine/core";
+import { IconSquareX, IconUserFilled } from "@tabler/icons-react";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import classes from "./styles.module.css";
+
+function VaultUserBox({
+  vaultId,
+  userEmail,
+  userPermissions,
+}: {
+  vaultId: number;
+  userEmail: string;
+  userPermissions: string[];
+}) {
+  const { colorScheme } = useMantineColorScheme();
+  const theme = useMantineTheme();
+  const user = useAppSelector((state) => state.user);
+
+  return (
+    <Box py={"sm"} px={"xs"} className={classes.VaultUserBox}>
+      <Group gap={"xs"}>
+        <IconUserFilled size={"1.75rem"} />
+        <Text size="lg">{userEmail}</Text>
+        {user.email !== userEmail && (
+          <Box ml={"auto"}>
+            <ActionIcon
+              variant="transparent"
+              onClick={() => {}}
+              color={
+                colorScheme == "light"
+                  ? theme.colors.red[7]
+                  : theme.colors.red[6]
+              }
+            >
+              <IconSquareX size={"1.5rem"} />
+            </ActionIcon>
+          </Box>
+        )}
+      </Group>
+      <Text mt={"sm"}>
+        <b>Permissions:</b> {userPermissions.join(", ")}
+      </Text>
+    </Box>
+  );
+}
+
+export default function VaultUsersList({ vaultId }: { vaultId: number }) {
+  const vaultUsersQuery = useQuery({
+    queryKey: ["vaultUsers", vaultId],
+    queryFn: () => listVaultUsers(Number(vaultId)),
+    retry: (failureCount: number, error: Error) => {
+      if (failureCount > 2) {
+        return false;
+      }
+      if (axios.isAxiosError(error)) {
+        if (
+          (error.response?.status ?? 500 >= 400) &&
+          (error.response?.status ?? 500 < 500)
+        ) {
+          return false;
+        }
+      }
+      return true;
+    },
+  });
+
+  return (
+    <>
+      {!vaultUsersQuery.isSuccess ? (
+        <Loader color="gray" />
+      ) : (
+        vaultUsersQuery.data.map((user) => (
+          <VaultUserBox
+            key={user.email}
+            vaultId={Number(vaultId)}
+            userEmail={user.email}
+            userPermissions={user.permissions}
+          />
+        ))
+      )}{" "}
+    </>
+  );
+}
--- frontend/src/pages/vault-users-page/index.tsx
+++ frontend/src/pages/vault-users-page/index.tsx
@@ -1,9 +1,72 @@
-import { Title } from "@mantine/core";
+import { retrieveVault } from "@/api/letuspass";
+import { ActionIcon, Box, Group, Loader, Text, Title } from "@mantine/core";
+import {
+  IconArrowLeft,
+  IconBriefcase2,
+  IconUserPlus,
+  IconUsersGroup,
+} from "@tabler/icons-react";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { useNavigate, useParams } from "react-router-dom";
+import VaultUsersList from "./VaultUsersList";
 
 function VaultUsersPage() {
+  const { vaultId } = useParams();
+  const navigate = useNavigate();
+
+  const vaultQuery = useQuery({
+    queryKey: ["vault", vaultId],
+    queryFn: () => retrieveVault(Number(vaultId)),
+    retry: (failureCount: number, error: Error) => {
+      if (failureCount > 2) {
+        return false;
+      }
+      if (axios.isAxiosError(error)) {
+        if (
+          (error.response?.status ?? 500 >= 400) &&
+          (error.response?.status ?? 500 < 500)
+        ) {
+          return false;
+        }
+      }
+      return true;
+    },
+  });
+
   return (
     <>
-      <Title>Vault Manage Users Page</Title>
+      <Group gap={"sm"} justify="flex-start">
+        <ActionIcon
+          variant="transparent"
+          color="dark"
+          onClick={() => navigate(-1)}
+        >
+          <IconArrowLeft size={"1.5rem"} />
+        </ActionIcon>
+        <IconBriefcase2 size={"1.75rem"} />
+        <Title order={2} fw={"lighter"}>
+          {vaultQuery.isSuccess ? (
+            vaultQuery.data?.name
+          ) : (
+            <Loader color="gray" />
+          )}
+        </Title>
+      </Group>
+
+      <Group mt={"lg"} mb={"sm"} gap={"sm"} justify="flex-start">
+        <IconUsersGroup size={"1.5rem"} />
+        <Text fz={"h3"} fw={"lighter"}>
+          Vault Users
+        </Text>
+        <Box ml={"auto"} mr={"xs"}>
+          <ActionIcon variant="transparent" color="dark" onClick={() => null}>
+            <IconUserPlus size={"1.5rem"} />
+          </ActionIcon>
+        </Box>
+      </Group>
+
+      <VaultUsersList vaultId={Number(vaultId)} />
     </>
   );
 }
