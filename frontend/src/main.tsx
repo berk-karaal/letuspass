42580651811774c3552e64f4845d12176145d04e
berk-karaal <karaalberk7@gmail.com>
2024-09-07 19:44:34 +0300

    feat(maint.tsx): remove react query devtools

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,7 +6,6 @@ import { Notifications } from "@mantine/notifications";
 import "@mantine/notifications/styles.css";
 import { IconEye, IconEyeOff } from "@tabler/icons-react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider as ReduxProvider } from "react-redux";
@@ -35,7 +34,6 @@ createRoot(document.getElementById("root")!).render(
         <ReduxProvider store={store}>
           <Notifications />
           <App />
-          <ReactQueryDevtools initialIsOpen={false} />
         </ReduxProvider>
       </QueryClientProvider>
     </MantineProvider>
