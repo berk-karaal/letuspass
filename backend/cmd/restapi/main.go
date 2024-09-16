d2b7b827d37ff7ba0e5dafaa4db2382f92a03a5e
berk-karaal <karaalberk7@gmail.com>
2024-09-16 23:48:21 +0300

    refactor: refactor setting up router

--- backend/cmd/restapi/main.go
+++ backend/cmd/restapi/main.go
@@ -4,7 +4,9 @@ import (
 	"log"
 	"os"
 
+	"github.com/berk-karaal/letuspass/backend/internal/common/logging"
 	"github.com/berk-karaal/letuspass/backend/internal/config"
+	"github.com/berk-karaal/letuspass/backend/internal/databases/postgres"
 	"github.com/berk-karaal/letuspass/backend/internal/router"
 	"github.com/joho/godotenv"
 )
@@ -25,6 +27,15 @@ func main() {
 	}
 	apiConfig := config.NewRestapiConfigFromEnv()
 
+	logger := logging.NewLogger(apiConfig.LogFile)
+
+	postgresDb, err := postgres.NewDB(postgres.GenerateDSN(
+		apiConfig.DbHost, apiConfig.DbUser, apiConfig.DbPassword, apiConfig.DbName,
+		apiConfig.DbPort, apiConfig.DbSSLMode, apiConfig.DbTimeZone))
+	if err != nil {
+		log.Fatal("Failed to connect to postgres database: ", err)
+	}
+
 	log.Printf("Starting rest api server.")
-	router.SetupRouter(apiConfig).Run()
+	router.SetupRouter(apiConfig, logger, postgresDb).Run()
 }
